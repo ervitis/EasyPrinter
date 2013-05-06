ad03761e531c6b56b9e3f4f46bcb0a1fc2dedf8e
v <vitomarti@gmail.com>
2013-05-07 00:57:06 +0200

    Added the string, menu and main properties and more default methods in MainActivity

--- src/com/easyprinter/MainActivity.java
+++ src/com/easyprinter/MainActivity.java
@@ -1,7 +1,11 @@
 package com.easyprinter;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity
 {
@@ -10,8 +14,7 @@ public class MainActivity extends Activity
    * @param savedInstanceState contains the activity's previously frozen state
    */
   @Override
-  protected void onCreate(Bundle savedInstanceState)
-  {
+  protected void onCreate(Bundle savedInstanceState){
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
   }
@@ -21,7 +24,7 @@ public class MainActivity extends Activity
    */
   @Override
   protected void onRestart(){
-	super.onRestart();
+		super.onRestart();
   }
   
   /**
@@ -29,7 +32,7 @@ public class MainActivity extends Activity
    */
   @Override
   protected void onStart(){
-	super.onStart();
+		super.onStart();
   }
   
   /**
@@ -37,7 +40,7 @@ public class MainActivity extends Activity
    */
   @Override
   protected void onResume(){
-	super.onResume();
+		super.onResume();
   }
   
   /**
@@ -45,7 +48,7 @@ public class MainActivity extends Activity
    */
   @Override
   protected void onPause(){
-	super.onPause();
+		super.onPause();
   }
   
   /**
@@ -53,7 +56,7 @@ public class MainActivity extends Activity
    */
   @Override
   protected void onStop(){
-	super.onStop();
+		super.onStop();
   }
   
   /**
@@ -61,6 +64,42 @@ public class MainActivity extends Activity
    */
   @Override
   protected void onDestroy(){
-	super.onDestroy();
+		super.onDestroy();
   }
+	
+	/**
+	 * Initialize the contents of the options menu
+	 * @param menu			the menu
+	 * @return					<code>true</code> to display the menu
+	 */
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu){
+		super.onCreateOptionsMenu(menu);
+		MenuInflater mInf = getMenuInflater();
+		mInf.inflate(R.menu.menu_main, menu);
+		return true;
+	}
+	
+	/**
+	 * Called when an item is selected from the menu
+	 * @param featuredId	the panel where the menu is in
+	 * @param item				the menu item selected
+	 * @return						<code>true</code> to finnish the selection or 
+	 *										<code>false</code> to perform the normal menu handling
+	 */
+	@Override
+	public boolean onMenuItemSelected(int featuredId, MenuItem item){
+		return super.onOptionsItemSelected(item);
+	}
+	
+	/**
+	 * Called when an activity launched exists giving the request code and the result code
+	 * @param requestCode	the request code to identify it
+	 * @param resultCode	the result code returned from the child activity
+	 * @param data				the data returned
+	 */
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data){
+		
+	}
 }
