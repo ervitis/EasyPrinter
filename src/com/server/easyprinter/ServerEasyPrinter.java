d2887fca25d77b404bcecd6b855f082554850ec1
v <vitomarti@gmail.com>
2013-05-13 03:47:02 +0200

    Correct bugs and try sending some files from client android

--- src/com/server/easyprinter/ServerEasyPrinter.java
+++ src/com/server/easyprinter/ServerEasyPrinter.java
@@ -28,9 +28,7 @@ public class ServerEasyPrinter {
 			ServerSocket listener = new ServerSocket(PORT);
 			Socket server;
 			
-			while ( (i++ < maxConnections) || (maxConnections == 0) ){
-				doComms connection;
-				
+			while ( (i++ < maxConnections) || (maxConnections == 0) ){				
 				server = listener.accept(); //wait
 				doComms c = new doComms(server);
 				Thread t = new Thread(c);
@@ -67,39 +65,37 @@ class doComms implements Runnable{
 			BufferedReader in = new BufferedReader(new InputStreamReader(server.getInputStream()));
 			PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(server.getOutputStream())));
 			
+			BufferedInputStream bis = new BufferedInputStream(server.getInputStream());
+			
 			while ( (line = in.readLine()) != null && !line.equals("FIN")){
-				if ( line.length() < 25 ){
+				if ( line.contains(";") ){
 					tempcrc = line.split(";");
-				}
 				
-				if ( tempcrc[0].equals("SEARCHING") ){
-					input += line;
-					
-					if ( input.endsWith(".txt") || input.endsWith("pdf") ){
-						fileName = input;
+					if ( tempcrc[0].equals("SEARCHING") ){
+						input += line;
+
+						crc_b = tempcrc[0].getBytes("utf-8");
+						crc32.update(crc_b);
+						crc32value = crc32.getValue();
+
+						crc32sendedvalue = Long.parseLong(tempcrc[1]);
+
+						if ( crc32sendedvalue == crc32value ){
+							//out.println("CONNECTED"); //send to the client
+							//instead of this, send the printer
+							PrintService printService = PrintServiceLookup.lookupDefaultPrintService();
+							String printerName = printService.getName();
+							out.println(printerName);
+
+							System.out.println("CRC fine");
+						}
+						else{
+							System.err.println("CRC corrupted");
+						}
+
+						System.out.println(input);
+						out.flush();
 					}
-
-					crc_b = tempcrc[0].getBytes("utf-8");
-					crc32.update(crc_b);
-					crc32value = crc32.getValue();
-
-					crc32sendedvalue = Long.parseLong(tempcrc[1]);
-
-					if ( crc32sendedvalue == crc32value ){
-						//out.println("CONNECTED"); //send to the client
-						//instead of this, send the printer
-						PrintService printService = PrintServiceLookup.lookupDefaultPrintService();
-						String printerName = printService.getName();
-						out.println(printerName);
-						
-						System.out.println("CRC fine");
-					}
-					else{
-						System.err.println("CRC corrupted");
-					}
-
-					System.out.println(input);
-					out.flush();
 				}
 				else{
 					//file
@@ -110,20 +106,52 @@ class doComms implements Runnable{
 			
 			if ( isFile ){
 				byte[] buffer = new byte[8192];
-				int n;
-				BufferedInputStream bis = new BufferedInputStream(server.getInputStream());
-				BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(fileName));
+				int n = 0;
 				
-				while ( (n = bis.read(buffer)) != -1 ){
+				//get the file name in line
+				String[] temp;
+				temp = line.split("#");
+				int nameSize = Integer.parseInt(temp[1]);
+				int fileSize = Integer.parseInt(temp[2]);
+				fileName = temp[0].substring(temp[0].length() - nameSize);
+				if ( fileName.endsWith(".txt") || fileName.endsWith(".pdf") ){
+					//sends an answer
+					out.println("CORRECT");
+					out.flush();
+					
+					System.out.println("File name: " + fileName);
+					
+					BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(fileName));
+
+					while ( n < fileSize ){
+						n += bis.read(buffer);
+					}
+					
+					//conversion
+					String t = new String(buffer, "UTF-8");
+					int c = Math.abs(n - fileSize);
+					System.out.println("Filesize: " + fileSize + ", n: " + n + ", c: " + c);
+					t = t.substring(c);
+					buffer = t.getBytes("utf-8");
+						
 					bos.write(buffer, 0, n);
+					bos.flush();
+					
+					out.println("CORRECT");
+					out.flush();
+					
+					//here is where i should check the crc code
+
+					File r = new File(fileName);
+					System.out.println("Received " + r.length());
+
+					bos.flush();
+					bis.close();
+					bos.close();
 				}
-				
-				File r = new File(fileName);
-				System.out.println("Received " + r.length());
-				
-				bos.flush();
-				bis.close();
-				bos.close();
+				else{
+					System.out.println("Archivo no soportado");
+				}
 			}
 			
 			System.out.println("Cerrando conexion");
